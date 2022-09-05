7898b16a58fa87fca4bda75fef3329ad444d2975
LEANDRO <leandroleo4916@gmail.com>
2022-09-05 16:24:55 -0300

    Added info deputado

--- app/src/main/java/com/example/portaldatransparencia/views/DeputadoActivity.kt
+++ app/src/main/java/com/example/portaldatransparencia/views/DeputadoActivity.kt
@@ -1,12 +1,19 @@
 package com.example.portaldatransparencia.views
 
+import android.R.attr.data
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.portaldatransparencia.R
 import com.example.portaldatransparencia.databinding.ActivityDeputadoBinding
+import com.example.portaldatransparencia.dataclass.IdDeputadoDataClass
 import com.example.portaldatransparencia.remote.ResultIdRequest
 import com.google.android.material.tabs.TabLayoutMediator
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
+
 
 class DeputadoActivity : AppCompatActivity() {
 
@@ -40,7 +47,9 @@ class DeputadoActivity : AppCompatActivity() {
             it?.let { result ->
                 when (result) {
                     is ResultIdRequest.Success -> {
-                        result.dado?.let { deputado -> }
+                        result.dado?.let { deputado ->
+                            addElementView(deputado)
+                        }
                     }
                     is ResultIdRequest.Error -> {
                         result.exception.message?.let { it -> }
@@ -51,5 +60,25 @@ class DeputadoActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun addElementView(item: IdDeputadoDataClass) {
+        binding.run {
+            Glide.with(application)
+                .load(item.dados.ultimoStatus.urlFoto)
+                .circleCrop()
+                .into(imageDeputado)
+            val age = age(item.dados.dataNascimento)
+            ("${item.dados.ultimoStatus.nome}, ${age} anos, nascido em " +
+                    "${item.dados.municipioNascimento} - ${item.dados.ufNascimento}. " +
+                    "Filiado ao partido ${item.dados.ultimoStatus.siglaPartido}").also { textDescription.text = it }
+        }
+    }
+
+    private fun age(birth: String): Int {
+        val date = LocalDate.parse(birth, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+        val dateCurrent = LocalDate.now()
+        val periodo = Period.between(date, dateCurrent)
+        return periodo.years
     }
 }
